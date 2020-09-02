8d149daacfa48612fab0f4af7377dd72c11d18a7
mannan0819 <mannan1994@hotmail.com>
2020-09-02 15:27:13 -0400

    Add files via upload

---
+++ build/precache-manifest.0c90d7da36341999f0335a1d5b3e024a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "589d651bd17547a60e424f16ebc67544",
+    "url": "/react-project/index.html"
+  },
+  {
+    "revision": "a9d4fb3a9ac241715cb4",
+    "url": "/react-project/static/css/main.8f7e200a.chunk.css"
+  },
+  {
+    "revision": "1b5e61b59251089ba9ff",
+    "url": "/react-project/static/js/2.65bf9245.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-project/static/js/2.65bf9245.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9d4fb3a9ac241715cb4",
+    "url": "/react-project/static/js/main.76b28076.chunk.js"
+  },
+  {
+    "revision": "f74eb8bc5ceedd0bc1e9",
+    "url": "/react-project/static/js/runtime-main.8a4c715b.js"
+  },
+  {
+    "revision": "b34f9e5a5f5d88425b38dc7665100c1b",
+    "url": "/react-project/static/media/webpage.b34f9e5a.jpg"
+  }
+]);
